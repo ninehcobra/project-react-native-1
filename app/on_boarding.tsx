d7dcc0fcec9e09e80dd53e1fc23817e80d1f806b
ninehcobra <ttbexinhtt2903@gmail.com>
2024-09-17 15:03:32 +0700

    feat: add prototyping component badge

--- app/on_boarding.tsx
+++ app/on_boarding.tsx
@@ -1,12 +1,24 @@
+import Badge from "@/components/prototyping_components/Badge";
 import Banner from "@/components/prototyping_components/Banner";
 import ContainerWrapper from "@/components/prototyping_components/ContainerWrapper";
 import Dialog from "@/components/prototyping_components/Dialog";
 import { Colors } from "@/constants/Colors";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import Toast from "react-native-toast-message";
-
+import Icon from "react-native-vector-icons/FontAwesome";
 export default function OnBoarding({ navigation }: { navigation: any }) {
   // navigation.setOptions({ headerShown: false });
+  const [number, setNumber] = useState<number>(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (number == 100) {
+        setNumber(0);
+      } else setNumber((prevNumber) => prevNumber + 1);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <ContainerWrapper>
@@ -17,7 +29,7 @@ export default function OnBoarding({ navigation }: { navigation: any }) {
           padding: 20,
         }}
       >
-        <Dialog />
+        <Badge number={number} size="large" icon={<Icon name="heart" />} />
       </View>
     </ContainerWrapper>
   );
--- components/prototyping_components/Badge.tsx
+++ components/prototyping_components/Badge.tsx
@@ -1,8 +1,66 @@
 import { Colors } from "@/constants/Colors";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { IconProps } from "react-native-vector-icons/Icon";
 
-export default function Badge(): React.ReactNode {
+export default function Badge({
+  size = "small",
+  backgroundColor,
+  color,
+  number,
+  icon,
+}: {
+  size?: "small" | "medium" | "large";
+  backgroundColor?: string;
+  color?: string;
+  number?: number;
+  icon?: React.ReactNode;
+}): React.ReactNode {
+  const sizeSmall = 20;
+  const sizeMedium = 30;
+  const sizeLarge = 40;
+
+  const badgeSize =
+    size == "small" ? sizeSmall : size == "medium" ? sizeMedium : sizeLarge;
+  const fontSize =
+    size == "small"
+      ? sizeSmall * 0.55
+      : size == "medium"
+      ? sizeMedium * 0.55
+      : sizeLarge * 0.55;
+
+  function formatNumber(num: number): string {
+    if (num <= 0) return "0";
+    if (num > 99) return "99+";
+    return num.toString();
+  }
   return (
-    <View style={{ backgroundColor: Colors.highlight.highlightColor_1 }}></View>
+    <View
+      style={{
+        backgroundColor:
+          backgroundColor != null
+            ? backgroundColor
+            : Colors.highlight.highlightColor_1,
+        height: badgeSize,
+        width: badgeSize,
+        borderRadius: badgeSize,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {icon != null ? (
+        icon
+      ) : number != null ? (
+        <Text
+          style={{
+            fontSize: formatNumber(number) == "99+" ? fontSize * 0.8 : fontSize,
+            color: color != null ? color : Colors.light.neutralColor_5,
+          }}
+        >
+          {formatNumber(number)}
+        </Text>
+      ) : (
+        ""
+      )}
+    </View>
   );
 }
