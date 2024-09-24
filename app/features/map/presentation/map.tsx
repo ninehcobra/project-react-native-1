590d261d178a0e46a0340901cf279382ea256a54
ninehcobra <ttbexinhtt2903@gmail.com>
2024-09-24 11:44:22 +0700

    fix: change logic

--- app/features/map/presentation/map.tsx
+++ app/features/map/presentation/map.tsx
@@ -100,6 +100,7 @@ export default function Map({
         selectedLocation.longitude,
         selectedRadius
       );
+
       refetch();
     }
   }, [queryData, selectedRadius]);
@@ -444,7 +445,7 @@ export default function Map({
           <View style={styles.modalContent}>
             <TouchableOpacity
               style={styles.closeButton}
-              onPress={() => setModalVisible(false)}
+              onPress={() => setSearchResult(false)}
             >
               <AntDesign name="close" size={24} color="white" />
             </TouchableOpacity>
@@ -549,207 +550,226 @@ export default function Map({
             <ScrollView
               style={{
                 marginTop: 12,
+                minHeight: 400,
               }}
               showsHorizontalScrollIndicator={false}
               showsVerticalScrollIndicator={false}
             >
-              {searchResponse && searchResponse.data.length > 0
-                ? searchResponse.data.map((place) => (
-                    <View key={place.id} style={{ marginBottom: 24 }}>
-                      <ScrollView
-                        showsHorizontalScrollIndicator={false}
-                        horizontal={true}
-                      >
-                        {place.images.length > 0 ? (
-                          place.images.map((image) => {
-                            return (
-                              <Image
-                                source={
-                                  image.url
-                                    ? { uri: image.url }
-                                    : require("../../../../assets/images/default_business.png")
-                                }
-                                defaultSource={require("../../../../assets/images/default_business.png")}
-                                onError={(error) =>
-                                  console.log("Image loading error:", error)
-                                }
-                                style={{
-                                  borderRadius: 12,
-                                  width: 165,
-                                  height: 165,
-                                  marginRight: 12,
-                                }}
-                              />
-                            );
-                          })
-                        ) : (
-                          <Image
-                            source={require("../../../../assets/images/default_business.png")}
-                            style={{
-                              borderRadius: 12,
-                              width: 165,
-                              height: 165,
-                              marginRight: 12,
-                            }}
-                          />
-                        )}
-                      </ScrollView>
-                      <View style={{ flexDirection: "row" }}>
-                        <View style={{ marginTop: 6, flex: 9 }}>
-                          <Text
-                            style={{
-                              ...typographyStyles.body_L,
-                              fontWeight: "700",
-                            }}
-                          >
-                            {place.name}
-                          </Text>
-                          <View
-                            style={{
-                              flexDirection: "row",
-                              alignItems: "center",
-                            }}
-                          >
-                            <Text
+              {searchResponse && searchResponse.data.length > 0 ? (
+                searchResponse.data.map((place) => (
+                  <View key={place.id} style={{ marginBottom: 24 }}>
+                    <ScrollView
+                      showsHorizontalScrollIndicator={false}
+                      horizontal={true}
+                    >
+                      {place.images.length > 0 ? (
+                        place.images.map((image) => {
+                          return (
+                            <Image
+                              source={
+                                image.url
+                                  ? { uri: image.url }
+                                  : require("../../../../assets/images/default_business.png")
+                              }
+                              defaultSource={require("../../../../assets/images/default_business.png")}
+                              onError={(error) =>
+                                console.log("Image loading error:", error)
+                              }
                               style={{
-                                ...typographyStyles.body_M,
-                                ...colorStyles.neutralColorDark_3,
+                                borderRadius: 12,
+                                width: 165,
+                                height: 165,
+                                marginRight: 12,
                               }}
-                            >
-                              {place.overallRating.toFixed(1)}
-                            </Text>
-
-                            <View
-                              style={{
-                                marginLeft: 4,
-                                marginBottom: 4,
-                                flexDirection: "row",
-                              }}
-                            >
-                              {[1, 2, 3, 4, 5].map((index) => {
-                                const starValue =
-                                  place.overallRating - index + 1;
-                                if (starValue >= 1) {
-                                  return (
-                                    <MaterialIcons
-                                      key={index}
-                                      name="star"
-                                      size={18}
-                                      color={Colors.support.warningColor_1}
-                                    />
-                                  );
-                                } else if (starValue > 0) {
-                                  return (
-                                    <MaterialIcons
-                                      key={index}
-                                      name="star-half"
-                                      size={18}
-                                      color={Colors.support.warningColor_1}
-                                    />
-                                  );
-                                } else {
-                                  return (
-                                    <MaterialIcons
-                                      key={index}
-                                      name="star-outline"
-                                      size={18}
-                                      color={Colors.support.warningColor_1}
-                                    />
-                                  );
-                                }
-                              })}
-                            </View>
-
-                            <Text
-                              style={{
-                                ...typographyStyles.body_M,
-                                ...colorStyles.neutralColorDark_3,
-                                marginLeft: 4,
-                              }}
-                            >{`(${place.totalReview}) . `}</Text>
-                            <Text
-                              style={{
-                                ...typographyStyles.body_M,
-                                ...colorStyles.neutralColorDark_3,
-                                marginLeft: 4,
-                              }}
-                            >{`${(place._distance / 1000).toFixed(
-                              1
-                            )} km`}</Text>
-                          </View>
-
+                            />
+                          );
+                        })
+                      ) : (
+                        <Image
+                          source={require("../../../../assets/images/default_business.png")}
+                          style={{
+                            borderRadius: 12,
+                            width: 165,
+                            height: 165,
+                            marginRight: 12,
+                          }}
+                        />
+                      )}
+                    </ScrollView>
+                    <View style={{ flexDirection: "row" }}>
+                      <View style={{ marginTop: 6, flex: 9 }}>
+                        <Text
+                          style={{
+                            ...typographyStyles.body_L,
+                            fontWeight: "700",
+                          }}
+                        >
+                          {place.name}
+                        </Text>
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                          }}
+                        >
                           <Text
                             style={{
                               ...typographyStyles.body_M,
                               ...colorStyles.neutralColorDark_3,
                             }}
                           >
-                            {place.category.name}
+                            {place.overallRating.toFixed(1)}
                           </Text>
 
                           <View
                             style={{
+                              marginLeft: 4,
+                              marginBottom: 4,
                               flexDirection: "row",
-                              alignItems: "center",
                             }}
                           >
-                            <Text
-                              style={{
-                                ...typographyStyles.body_M,
-                                ...colorStyles.supportColorSuccess_1,
-                              }}
-                            >
-                              Đang mở cửa .
-                            </Text>
-                            <Text
-                              style={{
-                                ...typographyStyles.body_M,
-                                marginLeft: 4,
-                              }}
-                            >
-                              Đóng cửa lúc 20:30
-                            </Text>
+                            {[1, 2, 3, 4, 5].map((index) => {
+                              const starValue = place.overallRating - index + 1;
+                              if (starValue >= 1) {
+                                return (
+                                  <MaterialIcons
+                                    key={index}
+                                    name="star"
+                                    size={18}
+                                    color={Colors.support.warningColor_1}
+                                  />
+                                );
+                              } else if (starValue > 0) {
+                                return (
+                                  <MaterialIcons
+                                    key={index}
+                                    name="star-half"
+                                    size={18}
+                                    color={Colors.support.warningColor_1}
+                                  />
+                                );
+                              } else {
+                                return (
+                                  <MaterialIcons
+                                    key={index}
+                                    name="star-outline"
+                                    size={18}
+                                    color={Colors.support.warningColor_1}
+                                  />
+                                );
+                              }
+                            })}
                           </View>
+
+                          <Text
+                            style={{
+                              ...typographyStyles.body_M,
+                              ...colorStyles.neutralColorDark_3,
+                              marginLeft: 4,
+                            }}
+                          >{`(${place.totalReview}) . `}</Text>
+                          <Text
+                            style={{
+                              ...typographyStyles.body_M,
+                              ...colorStyles.neutralColorDark_3,
+                              marginLeft: 4,
+                            }}
+                          >{`${(place._distance / 1000).toFixed(1)} km`}</Text>
                         </View>
-                        <View
+
+                        <Text
                           style={{
-                            flex: 2,
+                            ...typographyStyles.body_M,
+                            ...colorStyles.neutralColorDark_3,
                           }}
                         >
-                          <TouchableOpacity
-                            onPress={() =>
-                              onFlyTo({
-                                latitude: place.location.coordinates[1],
-                                longitude: place.location.coordinates[0],
-                                latitudeDelta: 0.005,
-                                longitudeDelta: 0.005,
-                              })
-                            }
+                          {place.category.name}
+                        </Text>
+
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Text
+                            style={{
+                              ...typographyStyles.body_M,
+                              ...colorStyles.supportColorSuccess_1,
+                            }}
                           >
-                            <View
-                              style={{
-                                height: 50,
-                                width: 50,
-                                borderRadius: 50,
-                                borderColor: Colors.dark.neutralColor_5,
-                                borderWidth: 1,
-                                marginTop: 12,
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                            >
-                              <FontAwesome6
-                                name="diamond-turn-right"
-                                size={24}
-                                color={Colors.highlight.highlightColor_1}
-                              />
-                            </View>
-                          </TouchableOpacity>
+                            Đang mở cửa .
+                          </Text>
+                          <Text
+                            style={{
+                              ...typographyStyles.body_M,
+                              marginLeft: 4,
+                            }}
+                          >
+                            Đóng cửa lúc 20:30
+                          </Text>
                         </View>
                       </View>
+                      <View
+                        style={{
+                          flex: 2,
+                        }}
+                      >
+                        <TouchableOpacity
+                          onPress={() =>
+                            onFlyTo({
+                              latitude: place.location.coordinates[1],
+                              longitude: place.location.coordinates[0],
+                              latitudeDelta: 0.005,
+                              longitudeDelta: 0.005,
+                            })
+                          }
+                        >
+                          <View
+                            style={{
+                              height: 50,
+                              width: 50,
+                              borderRadius: 50,
+                              borderColor: Colors.dark.neutralColor_5,
+                              borderWidth: 1,
+                              marginTop: 12,
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          >
+                            <FontAwesome6
+                              name="diamond-turn-right"
+                              size={24}
+                              color={Colors.highlight.highlightColor_1}
+                            />
+                          </View>
+                        </TouchableOpacity>
+                      </View>
                     </View>
-                  ))
-                : ""}
+                  </View>
+                ))
+              ) : (
+                <View>
+                  <Image
+                    source={require("../../../../assets/images/not-found.png")}
+                    style={{
+                      width: 200,
+                      height: 200,
+                      alignSelf: "center",
+                      marginTop: 100,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      ...typographyStyles.heading_H3,
+                      marginTop: 10,
+                      alignSelf: "center",
+                    }}
+                  >
+                    Không tìm thấy kết quả nào
+                  </Text>
+                </View>
+              )}
             </ScrollView>
           </View>
         </View>
